09ebf923f9df3b13dc1adab51aef1084e3367c3b
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-12-22 20:35:39 +0000

    indexJS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const   http = require('http'), //This module provides the HTTP server functionalities
         path = require('path'), //The path module provides utilities for working with file and directory paths
         express = require('express'), //This module allows this app to respond to HTTP requests, defines the routing and renders back the required content
